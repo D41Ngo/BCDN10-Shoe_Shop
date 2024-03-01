a92a4b04ff114ed3bff3063f040feca8455022e2
Dai <87512311+DaiTNgo@users.noreply.github.com>
2024-03-01 19:11:12 +0700

    setup

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./app.tsx";
+
+// document.getElementById("root") as HTMLElement
+// Giúp typescript biết chắc chắn là HTMLElement
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
